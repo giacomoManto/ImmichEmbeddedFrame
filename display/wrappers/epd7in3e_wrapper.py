09278997fecefb6114595e0682fda79e397192cb
Giacomo Mantovanelli <104456506+giacomoManto@users.noreply.github.com>
2025-04-01 23:11:39 -0400

    Add logger messages to display wrapper

--- display/wrappers/epd7in3e_wrapper.py
+++ display/wrappers/epd7in3e_wrapper.py
@@ -1,5 +1,6 @@
 from display.drivers import epd7in3e
 from ..base_display_manager import BaseDisplayManager
+from utils.logging_setup import setup_logger
 
 import os
 
@@ -8,6 +9,7 @@ class EPD7IN3E_MANAGER(BaseDisplayManager):
         self.WIDTH = epd7in3e.EPD_WIDTH
         self.HEIGHT = epd7in3e.EPD_HEIGHT
         self.epd = epd7in3e.EPD()
+        self.logger = setup_logger(__name__)
         
         
     def get_width(self):
@@ -17,17 +19,21 @@ class EPD7IN3E_MANAGER(BaseDisplayManager):
         return self.HEIGHT
     
     def init(self):
+        self.logger.info("Initializing EPD7IN3E display")
         self.epd.init()
     
     def clear(self):
+        self.logger.info("Clearing EPD7IN3E display")
         self.epd.Clear()
     
     def display(self, imagePath):
+        self.logger.info(f"Displaying image {imagePath} on EPD7IN3E display")
         with open(imagePath, "rb") as file:
             image = file.read()
         self.epd.display(image)
     
     def sleep(self):
+        self.logger.info("Sleeping EPD7IN3E display")
         self.epd.sleep()
         
     def get_act_path(self) -> str:
